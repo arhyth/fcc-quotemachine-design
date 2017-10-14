e4d3146ae159b032f4d0a261a88e833429bb7a9a
david <arhyth@gmail.com>
2017-10-15 01:37:27 +0800

    working commit before fcc comments

---
+++ index.js
@@ -0,0 +1,23 @@
+function reqListener() {
+    document.getElementById('quoteblock').innerHTML = this.response[0]['content'];
+    document.getElementById('author').innerHTML = textauthor = this.response[0]['title'];
+    document.getElementById('tweet').innerHTML = "tweet this";
+    var divit = document.createElement("div");
+    divit.innerHTML = this.response[0]['content'];
+    texto = divit.textContent;
+}
+var texto,textauthor;
+var quoteReq = new XMLHttpRequest();
+quoteReq.onload = reqListener;
+quoteReq.open("GET", "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1");
+quoteReq.send();
+quoteReq.responseType = "json";
+
+function tweetThis() {
+    window.open("https://twitter.com/intent/tweet?text="+encodeURIComponent(texto)+"   -"+textauthor);
+}
+
+function showAttrib() {
+    document.getElementById('attribution').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+'quote API by quotesondesign.com';
+}
+
