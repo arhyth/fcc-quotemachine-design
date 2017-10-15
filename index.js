0447f833c2c3f1299d438cab8da803d9f3543245
david <arhyth@gmail.com>
2017-10-15 23:09:05 +0800

    removed the use of flex containers

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 function reqListener() {
     document.getElementById('quoteblock').innerHTML = this.response[0]['content'];
     document.getElementById('author').innerHTML = textauthor = this.response[0]['title'];
-    document.getElementById('tweet').innerHTML = "tweet this";
     var divit = document.createElement("div");
     divit.innerHTML = this.response[0]['content'];
     texto = divit.textContent;
+    appendTweet();
 }
-var texto,textauthor;
-var quoteReq = new XMLHttpRequest();
-quoteReq.onload = reqListener;
-quoteReq.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1");
-quoteReq.send();
-quoteReq.responseType = "json";
+
+function appendTweet() {
+    document.getElementById('theblock').appendChild(document.createElement("div"));
+    document.getElementById('theblock').lastChild.outerHTML = '<div id="tweet" onclick="tweetThis()">tweet this</div>';
+}
 
 function tweetThis() {
     window.open("https://twitter.com/intent/tweet?text="+encodeURIComponent(texto)+"   -"+textauthor);
@@ -20,4 +19,15 @@ function tweetThis() {
 function showAttrib() {
     document.getElementById('attribution').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+'quote API by quotesondesign.com';
 }
+
+var texto,textauthor;
+var quoteReq = new XMLHttpRequest();
+
+quoteReq.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1");
+quoteReq.send();
+quoteReq.responseType = "json";
+quoteReq.onload = reqListener;
+
+//window.onload = appendTweet;
+
 
